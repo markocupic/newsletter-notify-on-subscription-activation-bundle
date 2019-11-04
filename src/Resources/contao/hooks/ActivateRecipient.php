6a57cb689323b6e0a7dd7c6105a3c68057504e82
Marko Cupic <m.cupic@gmx.ch>
2019-11-04 19:38:08 +0100

    Update ActivateRecipient.php

--- src/Resources/contao/hooks/ActivateRecipient.php
+++ src/Resources/contao/hooks/ActivateRecipient.php
@@ -13,9 +13,7 @@ class ActivateRecipient
     public function activateRecipient($strEmail, $arrAdd, $arrCids)
     {
         mail('m.cupic@gmx.ch', $strEmail,'s'); // Newsletter REcipient E-Mail
-        mail('m.cupic@gmx.ch', 'Newsletter REcipient Id',print_r($arrAdd,true));
+        mail('m.cupic@gmx.ch', 'Newsletter Recipient Id',print_r($arrAdd,true));
         mail('m.cupic@gmx.ch', 'Newsletter ID', print_r($arrCids,true));
-
-
     }
 }
