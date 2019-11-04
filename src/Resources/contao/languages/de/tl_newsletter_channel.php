ba12ce371562aa0d1ce679cd7245c6ab4a32f8c2
Marko Cupic <m.cupic@gmx.ch>
2019-11-04 19:33:13 +0100

    Create tl_newsletter_channel.php

---
+++ src/Resources/contao/languages/de/tl_newsletter_channel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marko
+ * Date: 04.11.2019
+ * Time: 19:06
+ */
+
+
+// Legends
+$GLOBALS['TL_LANG']['tl_newsletter_channel']['notify_on_subscription_activation_legend'] = "Benachrichtigungs-Einstellungen";
+
+
+// Fields
+$GLOBALS['TL_LANG']['tl_newsletter_channel']['notifyOnSubscriptionActivation'] = array('Benachrichtigung bei Newsletteranmeldung aktivieren', 'Bitte geben Sie an, ob bei der Aktivierung einer Newsletteranmeldung eine Nachricht versendet werden soll.');
+$GLOBALS['TL_LANG']['tl_newsletter_channel']['onSubscriptionActivationNotification'] = array('Benachrichtigung', 'Wählen Sie eine Benachrichtigung aus.');
