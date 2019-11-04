b7e9231e2845f51641a622cc3af5cdfd301072c0
Marko Cupic <m.cupic@gmx.ch>
2019-11-04 19:32:59 +0100

    dev

---
+++ src/Resources/contao/languages/de/tl_nc_notification.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marko
+ * Date: 04.11.2019
+ * Time: 19:29
+ */
+
+<?php
+
+/**
+ * Notification Center Default Email Web Plugin for Contao
+ * Copyright (c) 2008-2019 Marko Cupic
+ * @package notification-center-default-email-bundle
+ * @author Marko Cupic m.cupic@gmx.ch, 2019
+ * @link https://github.com/markocupic/notification-center-default-email-bundle
+ */
+
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['newsletter-notify-on-subscription-activation'] = 'Newsletter-Benachrichtigungen';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['notify-on-subscription-activation'] = array('Newsletter Anmeldung Aktivierung', 'Nachrichten bei Newsletter-Anmeldungs-Aktivierung versenden.');
